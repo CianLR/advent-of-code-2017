4a0ad46c4c5b3e3f74e87bf0b7a5d5872525aaf7
CianLR <cian.ruane1@gmail.com>
2017-12-19 08:49:50 +0000

    Add day 19 solutions

---
+++ day19/first.py
@@ -0,0 +1,61 @@
+import sys
+
+def get_start(grid, H, W):
+    for i in range(W):
+        if grid[0][i] == '|':
+            return (0, i)
+    return None
+
+def vec_add(a, b):
+    return tuple(x + y for x, y in zip(a, b))
+
+def l(grid, p):
+    return grid[p[0]][p[1]]
+
+def in_bounds(p, H, W):
+    return 0 <= p[0] < H and 0 <= p[1] < W
+
+def follow_path(grid, start, H, W):
+    out = ''
+    dirs = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+    direc = 0
+    cur = start
+    while cur:
+        v = l(grid, cur)
+        if v.isalpha():
+            out += v
+        nex = vec_add(cur, dirs[direc])
+        if in_bounds(nex, H, W) and l(grid, nex) != ' ':
+            cur = nex
+            continue
+        # Nothing in front
+        nex = vec_add(cur, dirs[(direc + 1) % 4])
+        if in_bounds(nex, H, W) and l(grid, nex) != ' ':
+            direc = (direc + 1) % 4
+            cur = nex
+            continue
+        nex = vec_add(cur, dirs[(direc - 1) % 4])
+        if in_bounds(nex, H, W) and l(grid, nex) != ' ':
+            direc = (direc - 1) % 4
+            cur = nex
+            continue
+        break
+    return out
+
+
+def main():
+    grid = []
+    for line in sys.stdin.readlines():
+        line = line.strip('\n')
+        if line:
+            grid.append(list(line))
+    H = len(grid)
+    W = len(grid[0])
+
+    start = get_start(grid, H, W)
+    print(follow_path(grid, start, H, W))
+
+
+
+if __name__ == '__main__':
+    main()
